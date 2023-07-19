053afc2008b28a5c286f5b96be47e22d8944e244
Heat-Space <109669255+Heat-Space@users.noreply.github.com>
2023-07-19 10:25:16 +0300

    Update Permissions.cs

--- Relations/Permissions.cs
+++ Relations/Permissions.cs
@@ -8,16 +8,16 @@ namespace Relations
 {
     public static class PluginPermissions
     {
-        public static readonly string kiss = "relation.kiss";
-        public static readonly string plunk = "relation.plunk";
-        public static readonly string marry = "relation.marry";
-        public static readonly string divorce = "relation.marry.divorce";
-        public static readonly string pat = "relation.pat";
-        public static readonly string beat = "relation.beat";
-        public static readonly string date = "relation.date";
-        public static readonly string sex = "relation.nsfw.sex";
-        public static readonly string marriage = "relation.manage.marriages";
-        public static readonly string rallow = "relation.rallow";
-        public static readonly string hug = "relation.hug";
+        public static readonly string kiss = "relations.kiss";
+        public static readonly string plunk = "relations.plunk";
+        public static readonly string marry = "relations.marry";
+        public static readonly string divorce = "relations.marry.divorce";
+        public static readonly string pat = "relations.pat";
+        public static readonly string beat = "relations.beat";
+        public static readonly string date = "relations.date";
+        public static readonly string sex = "relations.nsfw.sex";
+        public static readonly string marriage = "relations.manage.marriages";
+        public static readonly string rallow = "relations.rallow";
+        public static readonly string hug = "relations.hug";
     }
 }
